1c6ea08d608055e809d0e89a713ffbb4e6ec88e9
cristianABC <cristianarangodaza@gmail.com>
2017-04-21 23:39:45 -0500

    Herokuuu

--- src/App.js
+++ src/App.js
@@ -79,8 +79,8 @@ class App extends Component {
           <div>
             <img width="100"src={picture.image}/>
               <br/>
-              <img  width="50" src={picture.photoURL}alt={picture.displayName}/>
-              <text>Corazon</text>
+              <img  width="10" src={picture.photoURL}alt={picture.displayName}/>
+              <text>♥</text>
               <br/>
               <span>{picture.displayName}</span> 
           </div>
